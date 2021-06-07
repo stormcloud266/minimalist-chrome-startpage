8b3f9b9c75c671410a7a708da606d909dc3b5b55
Tawnee <allprt89@gmail.com>
2021-06-07 16:21:34 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,66 @@
+// elements
+const input = document.getElementById('search')
+const clock = document.getElementById('clock')
+const background = document.getElementById('background')
+const body = document.getElementsByTagName('BODY')[0]
+
+const image = {
+	defaultImage: './default.jpg',
+	nextImage: localStorage.getItem('bgImage'),
+	setImage: (image) => (background.style.backgroundImage = `url('${image}')`),
+}
+
+const start = () => {
+	setBg()
+	body.classList = 'fade'
+	setInterval(
+		(function setClock() {
+			const time = new Date().toLocaleTimeString().slice(0, -6)
+			clock.textContent = time
+			return setClock
+		})(),
+		500
+	)
+	input.addEventListener('keypress', googleSearch)
+	saveNextImage()
+}
+
+const setBg = () => {
+	if (!image.nextImage) {
+		image.setImage(image.defaultImage)
+	} else {
+		image.setImage(image.nextImage)
+	}
+}
+
+const toDataURL = (url) =>
+	fetch(url)
+		.then((response) => response.blob())
+		.then(
+			(blob) =>
+				new Promise((resolve, reject) => {
+					const reader = new FileReader()
+					reader.onloadend = () => resolve(reader.result)
+					reader.onerror = reject
+					reader.readAsDataURL(blob)
+				})
+		)
+
+const saveNextImage = () => {
+	toDataURL('https://source.unsplash.com/collection/2310706/2400x1600').then(
+		(dataUrl) => {
+			localStorage.setItem('bgImage', dataUrl)
+		}
+	)
+}
+
+const googleSearch = (e) => {
+	if (e.key === 'Enter') {
+		e.preventDefault()
+		const searchTerm = encodeURI(input.value)
+		const searchUrl = `https://www.google.com/search?q=${searchTerm}`
+		window.open(searchUrl, '_self')
+	}
+}
+
+start()
